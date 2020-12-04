a7103a0c8b1e2aa1a67e19c69c2dd74bf28c054d
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2020-12-05 04:43:46 +0700

    remove models wording

--- src/app/ucase/health.py
+++ src/app/ucase/health.py
@@ -1,7 +1,6 @@
 from flask_restful import Resource, reqparse, fields
 from src.app.helpers.rest import response
 import datetime
-# from src.models import model as db
 
 
 class HealthController(Resource):
