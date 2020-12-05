882a042a04fd2ea72f3837cf2f977581808b907b
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2020-12-05 22:31:13 +0700

    Create migrate command

--- src/cmd/migrate.py
+++ src/cmd/migrate.py
@@ -6,7 +6,8 @@ class Migrate(Base):
     """
 usage:
     migrate up [-p PATH] [-d DRIVER]
-    migrate down
+    migrate down [-p PATH] [-d DRIVER]
+    migrate create
 
 Command:
     up                                  start migrating
@@ -21,16 +22,18 @@ Options:
         APP_ROOT = os.path.join(os.path.dirname(__file__), '../..')
         default_path = self.args["--path"]
         default_driver = self.args["--driver"]
-
+        # setup default value
         if default_path == None:
             default_path = APP_ROOT+"/database/migration"
         if default_driver == None:
             default_driver = "pymysql"
+
         if self.args["up"]:
             try:
                 migration.run_migration(default_driver, default_path)
             except Exception as e:
                 print(e)
+                exit(1)
             else:
                 print("Migration Successfully")
                 exit(0)
@@ -40,7 +43,11 @@ Options:
                 rollback.rollback_migration(default_driver, default_path)
             except Exception as e:
                 print(e)
+                exit(1)
             else:
                 print("Rollback Successfully")
                 exit(0)
+
+        if self.args["create"]:
+            print("Coming Soon")
             exit(0)
