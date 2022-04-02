00888b76ee2646d272ae18137582bb22fa07c6c4
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2022-04-03 02:21:47 +0700

    casting and mysql connection

--- src/app/appctx/data.py
+++ src/app/appctx/data.py
@@ -1,4 +1,10 @@
 from flask import request
 
-def cast():
+def cast_json():
     return request.get_json(force=True)
+
+def cast_form(params):
+    return request.form.get(params)
+
+def cast_params(params):
+    return request.args.get(params)
