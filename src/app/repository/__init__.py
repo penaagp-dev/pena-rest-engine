0cc9e24d69afcd3e9af81330d7fabb22a0e158f0
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2022-04-03 14:50:24 +0700

    ok

--- src/app/repository/__init__.py
+++ src/app/repository/__init__.py
@@ -1,5 +1,5 @@
-from src.pkg.mariadb import get_cursor
+from src.pkg.mariadb import get_cursor, get_connection
 from src.app import app
 
-__conn___ = get_connection()
-__cursor__ = __conn___.cursor()
+# __conn___ = get_connection()
+# __cursor__ = __conn___.cursor()
--- src/app/repository/example.py
+++ src/app/repository/example.py
@@ -1,12 +1,5 @@
-from src.app.repository import __cursor__ as curr
+# from src.app.repository import __cursor__ as curr
 
 
 def insert(data):
-    try:
-        curr.execute("")
-    except Exception as e:
-        print("error:>", e)
-        return e
-    else:
-        print(data)
-        return data
+    pass
