e9d7bb1ad29336ae48cd73e125030fd02d35ba22
Sofyan Saputra <sofyan.saputra@otoraja.co.id>
2022-04-15 23:04:01 +0700

    repository example

--- src/app/repository/example.py
+++ src/app/repository/example.py
@@ -1,10 +1,10 @@
-# from src.app.repository import conn
+from src.app.repository import conn
 
-# cur = conn.cursor()
-# def insert():
-#     try:
-#         cur.execute("SELECT * FROM guest")
-#     except Exception as e:
-#         raise e
-#     else:
-#         return cur.fetchall()
+cur = conn.cursor()
+def insert():
+    try:
+        cur.execute("SELECT * FROM guest")
+    except Exception as e:
+        raise e
+    else:
+        return cur.fetchall()
